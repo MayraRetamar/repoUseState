f8bd40899f76e57ce6367813e4067230e752d399
mayraretamar <mayra.retamar@municipiodetandil.edu.ar>
2023-03-02 19:12:55 -0300

    codigo useState

---
+++ mi-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Cachorros from './Componentes/Evento/Cachorros';
+
+function App() {
+  return (
+    <div className="App">
+      <Cachorros/>
+    </div>
+  );
+}
+
+export default App;
---
+++ mi-app/src/Componentes/Evento/Cachorros.js
@@ -0,0 +1,39 @@
+import React, {useState} from  'react'
+import img1 from '../../imagenes/cachorro1.jpg'
+import img2 from '../../imagenes/cachorro2.jpeg'
+import img3 from '../../imagenes/cachorro3.jpg'
+
+const Cachorros = () => {
+
+    const [imagen, setImagen] = useState ({ 
+        imagen1 : null,
+        imagen2: null,
+        imagen3: null
+    })
+    
+    const cargarImg1 = () => { 
+        setImagen({...imagen, imagen1: img1})
+    }
+    const cargarImg2 = () => { 
+        setImagen({...imagen, imagen2: img2})
+    }
+    const cargarImg3 = () => { 
+        setImagen({...Cachorros, imagen3: img3})
+    }
+  
+  return (
+    <div>
+        
+        <button onClick={() => cargarImg1()} className= "btn-1" >Imágen 1</button>
+        <div>{imagen.imagen1} </div>
+        <button onClick={() => cargarImg2()} className= "btn-2">Imágen 2</button>
+        <div>{imagen.imagen2} </div>
+        <button onClick={() => cargarImg3()} className= "btn-3">Imágen 3</button>
+        <div>{imagen.imagen3} </div>
+
+      
+    </div>
+  )
+}
+
+export default Cachorros
