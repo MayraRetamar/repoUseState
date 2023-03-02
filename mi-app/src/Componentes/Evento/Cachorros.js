25f8a118444fe7acb9080651646d822ca0bb2b52
mayraretamar <mayra.retamar@municipiodetandil.edu.ar>
2023-03-02 20:32:45 -0300

    arreglo linea 21

--- mi-app/src/Componentes/Evento/Cachorros.js
+++ mi-app/src/Componentes/Evento/Cachorros.js
@@ -18,18 +18,18 @@ const Cachorros = () => {
         setImagen({...imagen, imagen2: img2})
     }
     const cargarImg3 = () => { 
-        setImagen({...Cachorros, imagen3: img3})
-    }
+        setImagen({...imagen, imagen3: img3}) 
+    } 
   
   return (
     <div>
         
         <button onClick={() => cargarImg1()} className= "btn-1" >Imágen 1</button>
-        <div>{imagen.imagen1} </div>
+        <div> {imagen.imagen1} </div>
         <button onClick={() => cargarImg2()} className= "btn-2">Imágen 2</button>
-        <div>{imagen.imagen2} </div>
+        <div> {imagen.imagen2} </div>
         <button onClick={() => cargarImg3()} className= "btn-3">Imágen 3</button>
-        <div>{imagen.imagen3} </div>
+        <div> {imagen.imagen3} </div>
 
       
     </div>
